fab4db8d210f890b0bf63f59b48ef503cd08e522
LiChongrui <94795082+LiChongrui@users.noreply.github.com>
2026-08-15 13:01:34 +0800

    3.0.3.6

--- .vuepress/config/bulletin.js
+++ .vuepress/config/bulletin.js
@@ -1,54 +1,54 @@
 module.exports = {
       body: [
-        {
-          type: "text",
-          content: `🎉🎉🎉 reco 主题 2.x `,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "QQ 群",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "GitHub",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "buttongroup",
-          children: [
-            {
-              text: "打赏",
-              link: "/docs/others/donate.html",
-            },
-          ],
-        },
-      ],
+      //   {
+      //     type: "text",
+      //     content: `🎉🎉🎉 reco 主题 2.x `,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "title",
+      //     content: "QQ 群",
+      //   },
+      //   {
+      //     type: "text",
+      //     content: `
+      //     <ul>
+      //       <li>QQ群1：1037296104</li>
+      //       <li>QQ群2：1061561395</li>
+      //       <li>QQ群3：962687802</li>
+      //     </ul>`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "title",
+      //     content: "GitHub",
+      //   },
+      //   {
+      //     type: "text",
+      //     content: `
+      //     <ul>
+      //       <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+      //       <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+      //     </ul>`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "buttongroup",
+      //     children: [
+      //       {
+      //         text: "打赏",
+      //         link: "/docs/others/donate.html",
+      //       },
+      //     ],
+      //   },
+      // ],
     }
